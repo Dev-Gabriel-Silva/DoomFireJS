b6aed47bbe0cb4a619fe5b5602664fa5d1a5b316
FalcoN <dev.gabriel2000@gmail.com>
2025-01-17 01:58:10 -0300

    FEATURE: Criada a função start. Atribuida ao onload do app para que execute todas as funções necessárias para o programa

--- src/script.js
+++ src/script.js
@@ -1,1 +1,19 @@
-console.log("hello world");
+const app = document.getElementById("app");
+
+const fireHeight = 10;
+const fireWidth = 10;
+const numberOfPixels = fireHeight * fireWidth;
+const firePixelIntensity = []; 
+
+
+function start(){
+    generateDataStructure();    
+}
+
+function generateDataStructure() {
+    for(let i = 0; i <= numberOfPixels;){
+        firePixelIntensity[i] = 0;
+    }
+}
+
+app.onload = start;
